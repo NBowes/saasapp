da58e08fcf7df075d6911d4728d43205140cb90f
Nathen Bowes <nathen.bowes@shopify.com>
2017-01-27 12:41:46 -0800

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about', to: 'pages#about'
+  resources :contact; # same as get 'contact/new', to: 'contact#new'
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  # See how all your routes lay out with 'rake routes'.
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with 'root'
   # root 'welcome#index'
 
   # Example of regular route:
