ccc2c04a31c5127589770efac00092a6d1d3b654
Nathen Bowes <nathen.bowes@shopify.com>
2017-01-31 15:07:59 -0800

    added mailgun to contact form

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#code from here http://guides.rubyonrails.org/action_mailer_basics.html under
+# smtp_settings
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'rocky-fjord-54516.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
