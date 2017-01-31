f9088bd393585b126a0bb3e42a44c0f8bcc353c6
Nathen Bowes <nathen.bowes@shopify.com>
2017-01-31 14:09:27 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/about', to: 'pages#about'
   resources :contacts, only: :create
-  get '/contact-us', to: 'contacts#new'
+  get 'contact-us', to: 'contacts#new'
   # same as get 'contact/new', to: 'contact#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with 'rake routes'.
